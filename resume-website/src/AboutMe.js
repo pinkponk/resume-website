b2d82f1a3f61534ea58e579502ce40f303a74ecc
Henrik Djurestål <telleport@hotmail.com>
2023-05-11 14:18:07 +0000

    skills done

--- resume-website/src/AboutMe.js
+++ resume-website/src/AboutMe.js
@@ -1,26 +1,102 @@
 // src/AboutMe.js
 import React from 'react';
 import './AboutMe.css';
+import SkillGrid from './SkillGrid';
 import profileImage from './assets/profile-image.jpg';
 
+
 const AboutMe = () => {
-    const skillsGroups = [
+    const skillGroups = [
         {
-            title: 'Programming',
-            strong: ['Python', 'Rust', 'Linux', 'Git', 'Docker', 'Kubernetes', 'Tensorflow', 'Pytorch', 'Bevy'],
-            others: ['Javascript', 'Web assembly', 'OpenAPI', 'FastAPI', 'Poetry', 'C++', 'Simulink', 'Vim', 'Matlab', 'C', 'Java', 'Webpack', 'Anaconda', 'Vscode', 'Pycharm'],
+            name: 'Programming',
+            color: '#FE938C',
+            skills: [
+                { name: 'Python', strong: true, description: 'Strong expertise in Python programming' },
+                { name: 'Rust', strong: true, description: 'Proficient in Rust programming' },
+                { name: 'Linux', strong: true, description: 'Extensive experience with Linux systems' },
+                { name: 'Git', strong: true, description: 'Skilled in using Git version control' },
+                { name: 'Docker', strong: true, description: 'Experienced in containerizing applications with Docker' },
+                { name: 'Kubernetes', strong: true, description: 'Proficient in orchestrating containers with Kubernetes' },
+                { name: 'Tensorflow', strong: true, description: 'Skilled in using Tensorflow for machine learning' },
+                { name: 'Pytorch', strong: true, description: 'Proficient in Pytorch for machine learning' },
+                { name: 'Bevy', strong: true, description: 'Experienced in using Bevy game engine' },
+                { name: 'Javascript', strong: false, description: 'Familiar with Javascript programming' },
+                { name: 'Web assembly', strong: false, description: 'Experience with Web assembly' },
+                { name: 'OpenAPI', strong: false, description: 'Familiar with OpenAPI for API documentation' },
+                { name: 'FastAPI', strong: false, description: 'Experience in developing web applications with FastAPI' },
+                { name: 'Poetry', strong: false, description: 'Familiar with Poetry for Python dependency management' },
+                { name: 'C++', strong: false, description: 'Experience with C++ programming' },
+                { name: 'Simulink', strong: false, description: 'Familiar with Simulink for modeling and simulation' },
+                { name: 'Vim', strong: false, description: 'Experience using Vim text editor' },
+                { name: 'Matlab', strong: false, description: 'Familiar with Matlab programming' },
+                { name: 'C', strong: false, description: 'Experience with C programming' },
+                { name: 'Java', strong: false, description: 'Familiar with Java programming' },
+                { name: 'Webpack', strong: false, description: 'Experience using Webpack for bundling' },
+                { name: 'Anaconda', strong: false, description: 'Familiar with Anaconda for Python environment management' },
+                { name: 'Vscode', strong: false, description: 'Experience using Visual Studio Code' },
+                { name: 'Pycharm', strong: false, description: 'Familiar with Pycharm IDE' },
+                { name: 'Build pipelines', strong: false, description: 'Experience with build pipelines' },
+                { name: 'Jupyter', strong: false, description: 'Familiar with Jupyter notebooks' },
+                { name: 'Bash', strong: false, description: 'Experience with Bash scripting' },
+            ],
         },
         {
-            title: 'Artificial Intelligence',
-            strong: ['Gaussian mixture models', 'unsupervised learning', 'seq2Seq', 'object detection', 'anomaly detection'],
-            others: ['Transformer', 'LSTM', 'reinforcement learning', 'GAN', 'Variational autoencoders'],
+            name: 'Artificial Intelligence',
+            color: '#E6B89C',
+            skills: [
+                { name: 'GMM', strong: true, description: 'Strong expertise in Gaussian Mixture Models' },
+                { name: 'Unsuper learning', strong: true, description: 'Proficient in unsupervised learning techniques' },
+                { name: 'Seq2Seq', strong: true, description: 'Experienced in sequence-to-sequence models' },
+                { name: 'Object detection', strong: true, description: 'Skilled in object detection algorithms' },
+                { name: 'Anomaly detection', strong: true, description: 'Proficient in anomaly detection techniques' },
+                { name: 'Transformer', strong: false, description: 'Familiar with Transformer models' },
+                { name: 'LSTM', strong: false, description: 'Experience using LSTM networks' },
+                { name: 'RL', strong: false, description: 'Familiar with Reinforcement Learning' },
+                { name: 'GAN', strong: false, description: 'Experience with Generative Adversarial Networks' },
+                { name: 'VAE', strong: false, description: 'Familiar with Variational Autoencoders' },
+                { name: 'NLP', strong: false, description: 'Experience with Natural Language Processing techniques and algorithms' },
+
+            ],
         },
         {
-            title: 'Robotics',
-            strong: ['ROS1', 'ROS2', 'Autodesk Inventor', 'Control Theory'],
-            others: ['Blender', '3D Printing', 'Solid Edge', 'Embedded Systems', 'Gazebo', 'Webots'],
+            name: 'Robotics',
+            color: '#EAD2AC',
+            skills: [
+                { name: 'ROS1', strong: true, description: 'Strong expertise in Robot Operating System 1' },
+                { name: 'ROS2', strong: true, description: 'Proficient in Robot Operating System 2' },
+                { name: 'Inventor', strong: true, description: 'Experienced in using Autodesk Inventor' },
+                { name: 'Control Theory', strong: true, description: 'Skilled in Control Theory for robotics' },
+                { name: 'Blender', strong: false, description: 'Familiar with Blender for 3D modeling' },
+                { name: '3D Printing', strong: false, description: 'I have both a FDM and a Resin printer at home. I got my first 3d printer 8 years ago.' },
+                { name: 'Solid Edge', strong: false, description: 'Familiar with Solid Edge for CAD' },
+                { name: 'Embedded', strong: false, description: 'Experience with embedded systems' },
+                { name: 'Gazebo', strong: false, description: 'Familiar with Gazebo for robot simulation' },
+                { name: 'Webots', strong: false, description: 'Experience with Webots for robot simulation' },
+                { name: 'Raspberry Pi', strong: false, description: 'Experience with Raspberry Pi for embedded systems' },
+            ],
         },
     ];
+
+
+    const SkillLegend = () => {
+        return (
+            <div className="skill-legend">
+                <div className="legend-item">
+                    <div className="legend-group">
+                        <span className="legend-star">⭐</span> Strong Skills
+                    </div>
+                </div>
+                <div className="legend-item">
+                    {skillGroups.map((group) => (
+                        <div key={group.name} className="legend-group">
+                            <div className="legend-color" style={{ backgroundColor: group.color }}></div>
+                            {group.name}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    };
 
     return (
         <section className="about-me" id="about-me">
@@ -29,25 +105,13 @@ const AboutMe = () => {
                 <img src={profileImage} alt="Profile" />
                 <div className="about-me-text">
                     <p>Lorem ipsum dolor sit amet...</p>
-                    <div className="skills">
-                        {skillsGroups.map((group, index) => (
-                            <div key={index} className="skills-group">
-                                <h4>{group.title}</h4>
-                                <ul>
-                                    {group.strong.map((skill, index) => (
-                                        <li key={index} className="strong">{skill}</li>
-                                    ))}
-                                    {group.others.map((skill, index) => (
-                                        <li key={index} className="others">{skill}</li>
-                                    ))}
-                                </ul>
-                            </div>
-                        ))}
-                    </div>
+                    <SkillGrid skillGroups={skillGroups} />
+                    <SkillLegend />
                 </div>
             </div>
         </section>
     );
 };
+
 
 export default AboutMe;
---
+++ resume-website/src/SkillGrid.js
@@ -0,0 +1,108 @@
+// src/SkillGrid.js
+import React, { useEffect, useState } from 'react';
+import './SkillGrid.css';
+
+const SkillGrid = ({ skillGroups }) => {
+    const [skills, setSkills] = useState([]);
+    const [selected, setSelected] = useState([]);
+    const [win, setWin] = useState(false);
+    const [gridSize, setGridSize] = useState(0);
+    const [hasWon, setHasWon] = useState(false);
+
+    useEffect(() => {
+        let concatSkills = [];
+        skillGroups.forEach((group) => {
+            concatSkills = concatSkills.concat(group.skills.map((skill) => ({ ...skill, category: group.name })));
+        });
+
+        // concatSkills.sort(() => Math.random() - 0.5);
+        setSkills(concatSkills);
+
+        // Calculate the grid size dynamically
+        const calculatedGridSize = Math.ceil(Math.sqrt(concatSkills.length));
+        setGridSize(calculatedGridSize);
+    }, [skillGroups]);
+
+    useEffect(() => {
+        const checkWin = () => {
+            for (let row = 0; row < gridSize; row++) {
+                let rowSelected = true;
+                for (let col = 0; col < gridSize; col++) {
+                    if (!selected.includes(row * gridSize + col)) {
+                        rowSelected = false;
+                        break;
+                    }
+                }
+                if (rowSelected) return true;
+            }
+            for (let col = 0; col < gridSize; col++) {
+                let colSelected = true;
+                for (let row = 0; row < gridSize; row++) {
+                    if (!selected.includes(row * gridSize + col)) {
+                        colSelected = false;
+                        break;
+                    }
+                }
+                if (colSelected) return true;
+            }
+            return false;
+        };
+
+        if (!hasWon) {
+            const win = checkWin();
+            if (win) {
+                setWin(true);
+                setHasWon(true);
+            }
+        }
+    }, [selected, gridSize, hasWon]);
+
+    const handleClick = (index) => {
+        setSelected((prevSelected) => {
+            if (prevSelected.includes(index)) {
+                return prevSelected.filter((i) => i !== index);
+            } else {
+                return [...prevSelected, index];
+            }
+        });
+    };
+
+    const getBackgroundColor = (skill) => {
+        const group = skillGroups.find((group) => group.name === skill.category);
+        return group.color;
+    };
+
+    return (
+        <>
+            <div className={`skill-grid${win ? ' blurred' : ''}`}>
+                {skills.map((skill, index) => (
+                    <div
+                        key={skill.name}
+                        className={`skill${selected.includes(index) ? ' selected' : ''}`}
+                        onClick={() => handleClick(index)}
+                        style={{
+                            backgroundColor: getBackgroundColor(skill),
+                            width: `calc(${100 / gridSize}% - 2px)`,
+                            // height: `calc(${100 / gridSize}% - 2px)`
+                        }}
+                    >
+                        <span className={`skill-name`}>
+                            {skill.strong ? '⭐ ' : ''}{skill.name}
+                        </span>
+                        <div className="skill-tooltip">{skill.description}</div>
+                    </div>
+                ))}
+            </div>
+            {win && (
+                <div className="win-message">
+                    You win!
+                    <button className="close-button" onClick={() => setWin(false)}>
+                        Close
+                    </button>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default SkillGrid;
